5887f71593eda3ba156076684294dcc9436fb803
Adele Lin <aidiaol@zillowgroup.com>
2024-03-01 11:36:53 -0800

    clean up and add logger

--- Sources/XCMetricsBackendLib/UploadMetrics/Repository/LogFileRepositoryFactory.swift
+++ Sources/XCMetricsBackendLib/UploadMetrics/Repository/LogFileRepositoryFactory.swift
@@ -21,7 +21,6 @@ import Foundation
 import Vapor
 
 struct LogFileRepositoryFactory {
-
     static func makeWithConfiguration(config: Configuration, logger: Logger) -> LogFileRepository {
         if config.useGCSLogRepository {
             logger.info("Initializing GCS LogFileRepository")
@@ -33,7 +32,7 @@ struct LogFileRepositoryFactory {
         }
         if config.useS3LogRepository {
             logger.info("Initializing S3 LogFileRepository")
-            guard let s3Repository = LogFileS3Repository(config: config) else {
+            guard let s3Repository = LogFileS3Repository(config: config, logger: logger) else {
                 preconditionFailure("AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, XCMETRICS_S3_BUCKET and " +
                     "XCMETRICS_S3_REGION are required when XCMETRICS_USE_S3_REPOSITORY is used")
             }
@@ -41,5 +40,4 @@ struct LogFileRepositoryFactory {
         }
         return LocalLogFileRepository()
     }
-
 }
--- Sources/XCMetricsBackendLib/UploadMetrics/Repository/LogFileS3Repository.swift
+++ Sources/XCMetricsBackendLib/UploadMetrics/Repository/LogFileS3Repository.swift
@@ -21,6 +21,7 @@ import AWSClientRuntime
 import AWSS3
 import ClientRuntime
 import Foundation
+import Logging
 import Vapor
 
 /// `LogFileRepository` that uses Amazon S3 to store and fetch logs
@@ -28,8 +29,9 @@ struct LogFileS3Repository: LogFileRepository {
     let bucketName: String
     let group: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
     let s3Client: S3Client
+    let logger: Logger
 
-    init?(bucketName: String, regionName: String) {
+    init?(bucketName: String, regionName: String, logger: Logger) {
         var client: S3Client?
         if Environment.get("AWS_PROFILE") != nil {
             // Initialize S3 client with SSOCredentialsProvider
@@ -40,44 +42,44 @@ struct LogFileS3Repository: LogFileRepository {
                 )
             )
         } else {
-            // Initialize S3 client with just the region
             client = try? S3Client(region: regionName)
         }
 
         guard let s3Client = client else {
-            print("Failed to initialize S3 client")
+            logger.error("[LogFileS3Repository] Failed to initialize S3 client")
             return nil
         }
 
         self.s3Client = s3Client
         self.bucketName = bucketName
+        self.logger = logger
 
-        // Verify the client
-        let promise = group.next().makePromise(of: Void.self)
-        Task {
-            do {
-                let response = try await s3Client.listBuckets(input: ListBucketsInput()) // This will list all available S3 buckets
-                promise.succeed(())
-                print("Available buckets: \(String(describing: response.buckets))")
-            } catch {
-                print("Unable to retrieve the list of buckets, error: \(error.localizedDescription)")
-                promise.fail(error)
-            }
-        }
-        do {
-            try promise.futureResult.wait()
-        } catch {
-            print("Error waiting for promise: \(error)")
-        }
+//         Verify the client
+        //  let promise = group.next().makePromise(of: Void.self)
+        //  Task {
+        //      do {
+        //          let response = try await s3Client.listBuckets(input: ListBucketsInput()) // This will list all available S3 buckets
+        //          promise.succeed(())
+        //          print("Available buckets: \(String(describing: response.buckets))")
+        //      } catch {
+        //          print("Unable to retrieve the list of buckets, error: \(error.localizedDescription)")
+        //          promise.fail(error)
+        //      }
+        //  }
+        //  do {
+        //      try promise.futureResult.wait()
+        //  } catch {
+        //      print("Error waiting for promise: \(error)")
+        //  }
     }
 
-    init?(config: Configuration) {
+    init?(config: Configuration, logger: Logger) {
         guard let bucketName = config.s3Bucket,
               let regionName = config.s3Region
         else {
             return nil
         }
-        self.init(bucketName: bucketName, regionName: regionName)
+        self.init(bucketName: bucketName, regionName: regionName, logger: logger)
     }
 
     func put(logFile: File) throws -> URL {
@@ -96,16 +98,17 @@ struct LogFileS3Repository: LogFileRepository {
             do {
                 let _ = try await self.s3Client.putObject(input: input)
                 promise.succeed(())
-                print("File upload successful")
+                logger.info("[LogFileS3Repository] Log file: '\(logFile.filename)' uploaded successfully to S3 bucket: '\(bucketName)'")
             } catch {
-                print("Error uploading file: \(error)")
+                logger.error("[LogFileS3Repository] Error uploading log file: '\(logFile.filename)' to S3 bucket: '\(bucketName)', Error: \(error)")
                 promise.fail(error)
             }
         }
+
         do {
             try promise.futureResult.wait()
         } catch {
-            print("Error waiting for promise: \(error)")
+            logger.error("[LogFileS3Repository] Error waiting for the completion of the file upload for log file: '\(logFile.filename)' to S3 bucket: '\(bucketName)', Error: \(error)")
         }
 
         guard let url = URL(string: "s3://\(bucketName)/\(logFile.filename)") else {
@@ -121,6 +124,7 @@ struct LogFileS3Repository: LogFileRepository {
         }
         let fileName = logURL.lastPathComponent
 
+        logger.info("[LogFileS3Repository] get \(logURL), bucket: \(bucket), object: \(fileName)")
         let input = GetObjectInput(
             bucket: bucket,
             key: fileName
@@ -128,19 +132,22 @@ struct LogFileS3Repository: LogFileRepository {
 
         let promise = group.next().makePromise(of: Data.self)
         Task {
-            let output = try await self.s3Client.getObject(input: input)
+            do {
+                let output = try await self.s3Client.getObject(input: input)
 
-            // Get the data stream object. Return immediately if there isn't one.
-            guard let body = output.body,
-                  let data = try await body.readData()
-            else {
-                promise.fail(RepositoryError.unexpected(message: "There was an error downloading file \(logURL)"))
-                return
+                // Get the data stream object. Return immediately if there isn't one.
+                guard let body = output.body, let data = try await body.readData() else {
+                    promise.fail(RepositoryError.unexpected(message: "There was an error fetching file \(logURL)"))
+                    return
+                }
+                promise.succeed(data)
+            } catch {
+                logger.error("[LogFileS3Repository] Failed to fetch file: \(error)")
+                promise.fail(error)
             }
-            promise.succeed(data)
         }
-        let data = try promise.futureResult.wait()
 
+        let data = try promise.futureResult.wait()
         let tmp = try TemporaryFile(creatingTempDirectoryForFilename: "\(UUID().uuidString).xcactivitylog")
         try data.write(to: tmp.fileURL)
         return LogFile(remoteURL: logURL, localURL: tmp.fileURL)
